6ad6813ad100005d0923f1bcf7080802458cbab4
ogius <kowalewski.olgierd@gmail.com>
2025-02-13 11:46:30 +0100

    added common decode and encode jwt abstraction

---
+++ api/common/jwt.go
@@ -0,0 +1,61 @@
+package common
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var (
+	errJwtTokenIsEmpty   error = errors.New("missing jwt token")
+	errJwtTokenIsInvalid error = errors.New("invalid jwt token")
+)
+
+type JwtClaims map[string]any
+
+type JwtConfig struct {
+	Secret     interface{}
+	SignMethod *jwt.SigningMethodHMAC // jwt.SigningMethodHS256
+}
+
+func NewJwtConfig(secret string) JwtConfig {
+	return JwtConfig{
+		Secret:     []byte(secret),
+		SignMethod: jwt.SigningMethodHS256,
+	}
+}
+
+func (config *JwtConfig) EncodeJwt(now time.Time, claims JwtClaims, expire time.Duration) string {
+	claims["exp"] = now.Add(expire).Unix()
+
+	unsignedToken := jwt.NewWithClaims(config.SignMethod, jwt.MapClaims(claims))
+	token, err := unsignedToken.SignedString(config.Secret)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	return string(token)
+}
+
+func (config *JwtConfig) DecodeJwt(rawToken string) (JwtClaims, error) {
+	if rawToken == "" {
+		return nil, errJwtTokenIsEmpty
+	}
+
+	token, err := jwt.Parse(string(rawToken), func(token *jwt.Token) (interface{}, error) {
+		return config.Secret, nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, errJwtTokenIsInvalid
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		log.Panic("this should always be claims but it isn't")
+	}
+
+	return JwtClaims(claims), nil
+}
